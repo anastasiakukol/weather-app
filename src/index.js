447c717f360a4d284559c205e6726419ee3c8e3b
anastasiakukol <anastasiakukol@gmail.com>
2022-07-31 20:42:07 +0300

    Added daily forecast using API

--- src/index.js
+++ src/index.js
@@ -105,22 +105,35 @@ function showCelsiusTemp(event) {
   fahrenheitlink.classList.remove("active");
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Mon", "Tue", "Wed", "Thu", "Sat"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-            <h6>${day}</h6>
-            <img src="images/Sun.png" alt="bright sun" width="30px" />
-            <span class="week-temperature">
-              15/21 ℃
+            <h6>${formatDay(forecastDay.dt)}</h6>
+            <img src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" width="40px" />
+            <span class="week-temperature-max">
+              ${Math.round(forecastDay.temp.max)}°/
+            </span>
+            <span class="week-temperature-min">
+              ${Math.round(forecastDay.temp.min)}°
             </span>
           </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
