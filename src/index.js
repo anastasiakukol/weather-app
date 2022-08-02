112d17b334bc0170563ab5fe7de52f63e1f4c898
anastasiakukol <anastasiakukol@gmail.com>
2022-08-02 21:31:06 +0300

    Reorganized structure of code,added background image and added comments
    Corrected function for weather slogan
    Changed some css properties

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Defining current day, date and time
 let now = new Date();
 function formatDate(newDate) {
   let days = [
@@ -36,6 +37,7 @@ function formatDate(newDate) {
 }
 console.log(formatDate(now));
 
+//Function for searching the city using API
 function search(city) {
   let apiKey = "5f91a3ee1e9a25ad7ffa3c41adb88170";
   let units = "metric";
@@ -48,9 +50,7 @@ function submit(event) {
   let cityElement = document.querySelector("#search-city").value;
   search(cityElement);
 }
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", submit);
-
+//Function for getting weather data using API
 function showTemp(response) {
   document.querySelector("h2").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -80,7 +80,7 @@ function showTemp(response) {
   getForecast(response.data.coord);
   changeForecastSlogan(descriptionElement);
 }
-
+//Function for getting current geolocation using API
 function showPosition(position) {
   let apiKey = "5f91a3ee1e9a25ad7ffa3c41adb88170";
   let units = "metric";
@@ -90,7 +90,7 @@ function showPosition(position) {
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
-
+//Function for converting celsius to fahrenheit
 function showFahrenheitTemp(event) {
   event.preventDefault();
   let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
@@ -98,21 +98,21 @@ function showFahrenheitTemp(event) {
   celsiuslink.classList.remove("active");
   fahrenheitlink.classList.add("active");
 }
-
+//Function for converting fahrenheit to celsius
 function showCelsiusTemp(event) {
   event.preventDefault();
   document.querySelector("#temperature").innerHTML = Math.round(celsiusTemp);
   celsiuslink.classList.add("active");
   fahrenheitlink.classList.remove("active");
 }
-
+//Function for getting days of week into the forecast
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
-
+//Function for getting forecast
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -139,6 +139,7 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+//Function for getting location for forecast using API
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "70baea25d03a56fd5d768cd48a5e4af2";
@@ -147,27 +148,29 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
+//Function for changing slogan depending on weather description
 function changeForecastSlogan(descriptionElement) {
   let forecastSlogan = document.querySelector("#slogan");
   if (
-    descriptionElement === "moderate rain" ||
-    descriptionElement === "overcast clouds" ||
-    descriptionElement === "broken clouds" ||
-    descriptionElement === "shower rain"
+    descriptionElement.innerHTML === "moderate rain" ||
+    descriptionElement.innerHTML === "overcast clouds" ||
+    descriptionElement.innerHTML === "broken clouds" ||
+    descriptionElement.innerHTML === "shower rain" ||
+    descriptionElement.innerHTML === "light rain"
   ) {
     forecastSlogan.innerHTML = "Don't forget your umbrella ☂️";
   } else {
     if (
-      descriptionElement === "scattered clouds" ||
-      descriptionElement === "few clouds" ||
-      descriptionElement === "clear sky"
+      descriptionElement.innerHTML === "scattered clouds" ||
+      descriptionElement.innerHTML === "few clouds" ||
+      descriptionElement.innerHTML === "clear sky"
     ) {
       forecastSlogan.innerHTML = "Don't forget your sunglasses 👓";
     } else {
       if (
-        descriptionElement === "snow" ||
-        descriptionElement === "light snow" ||
-        descriptionElement === "heavy snow"
+        descriptionElement.innerHTML === "snow" ||
+        descriptionElement.innerHTML === "light snow" ||
+        descriptionElement.innerHTML === "heavy snow"
       ) {
         forecastSlogan.innerHTML = "Don't forget your scarf 🧣";
       } else {
@@ -176,6 +179,11 @@ function changeForecastSlogan(descriptionElement) {
     }
   }
 }
+
+//Variables out of the functions
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submit);
 
 let celsiusTemp = null;
 
